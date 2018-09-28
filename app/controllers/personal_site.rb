2d6de18125910f91976f2d5467c63f5305bc8554
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-28 14:47:25 -0600

    css

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -9,11 +9,12 @@ require 'rack'
 require 'rack'
 
 class PersonalSite
-  
+
   def self.call(env)
     case env["PATH_INFO"]
     when '/' then index
     when '/about' then about
+    when '/main.css' then css
     else
       error
     end
@@ -34,4 +35,13 @@ class PersonalSite
   def self.render_view(page, code = '200')
     [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
   end
+
+  def self.css
+    render_static('main.css')
+  end
+
+  def self.render_static(asset)
+    [200, {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
+  end
+  
 end
