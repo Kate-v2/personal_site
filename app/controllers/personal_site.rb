94566dc04b3f52382319079ed44e2358671ffa1f
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-28 14:00:24 -0600

    Updating capybara save path

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,9 +1,14 @@
 
+require 'pry'
+
 require 'rack'
 
 
 class PersonalSite
+
   def self.call(env)
+    # binding.pry
     ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
   end
+
 end
