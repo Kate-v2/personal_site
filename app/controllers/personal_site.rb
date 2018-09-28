3977ba1041b9b90fbe849c61d2d1d99aa955d201
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-28 14:08:27 -0600

    dynamic webpage

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,11 +4,12 @@ require 'pry'
 require 'rack'
 
 
+require 'rack'
+
 class PersonalSite
 
   def self.call(env)
-    # binding.pry
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
   end
-
+  
 end
